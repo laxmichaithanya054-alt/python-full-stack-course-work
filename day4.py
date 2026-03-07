0d51f3460eaefd918295db39d1f90ca5d1f1b4f1
Laxmichaithanya <laxmichaithanya054@gmail.com>
2026-03-07 17:05:15 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,21 @@
+fingerprint = True
+
+if fingerprint:
+    print("Phone Unlocked")
+
+password = "1234"
+
+if password == "1234":
+    print("Login Successful")
+else:
+    print("Wrong Password")
+
+
+units = 120
+
+if units < 100:
+    print("Low bill")
+elif units < 200:
+    print("Medium bill")
+else:
+    print("High bill")
