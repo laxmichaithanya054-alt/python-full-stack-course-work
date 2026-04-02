243e85f2777cd4882e8b9257b98cd86ee379d217
Laxmichaithanya <laxmichaithanya054@gmail.com>
2026-04-02 22:00:02 +0530

    Add files via upload

---
+++ DAY 3.py
@@ -0,0 +1,89 @@
+
+price = float(input("enter the price: "))
+                    
+
+price = float (input("enter the price: "))
+                    
+enter the price: 20
+price
+                    
+20.0
+'anjusree,himaja,himabindu'.split(',')
+                    
+['anjusree', 'himaja', 'himabindu']
+names = input("Enter thr name: ")
+                    
+Enter thr name: chaitanya sai varun
+name
+            
+names
+                    
+'chaitanya sai varun'
+name = input("enter the name: ").split()
+                    
+enter the name: chaitanya varun sai
+names
+                    
+'chaitanya sai varun'
+numbers = input("Enter the numbers: ").split()
+                    
+Enter the numbers: 4 5 6 7 8
+numbers
+                    
+['4', '5', '6', '7', '8']
+numbers=list(map(int,input("Enter the numbers: ").split()))
+                    
+Enter the numbers: 2 3 45 67
+numbers
+                    
+[2, 3, 45, 67]
+KeyboardInterrupt
+number=tuple(map(int,input("Enter the numbers: ").split()))
+                    
+Enter the numbers: 6.6 6.9 5.6 
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    number=tuple(map(int,input("Enter the numbers: ").split()))
+ValueError: invalid literal for int() with base 10: '6.6'
+>>> number=tuple(map(int,input("Enter the number: ").split()))
+...                     
+Enter the number: 2 3 3 4
+>>> numbers
+...                     
+[2, 3, 45, 67]
+>>> numbers=tuple(map(int,input("Enter the number: ").split()))
+...                     
+Enter the number: 3 4 35 6
+>>> numbers
+...                     
+(3, 4, 35, 6)
+>>> number=tuple(map(float,input("Enter the number: ").split()))
+...                     
+Enter the number: 6.7 8.4 4.6
+>>> numbers
+...                     
+(3, 4, 35, 6)
+>>> number=set(map(int,input("Enter the number: ").split()))
+...                     
+Enter the number: 4 5 67  7
+>>> numbers
+...                     
+(3, 4, 35, 6)
+>>> number=set(input("Enter the names: ").split())
+...                     
+Enter the names: sai chaitanya
+>>> number
+...                     
+{'sai', 'chaitanya'}
+>>> numbers=set(map(int,input("Enter the numbers: ").split()))
+...                     
+Enter the numbers: 4 5 6 7
+>>> numbers
+...                     
+{4, 5, 6, 7}
+>>> d=eval (input("enter the input: "))
+...                     
+enter the input: [1,2,3,4]
+>>> d
+...                     
+[1, 2, 3, 4]
