a7b296de4b8180befaa6f7f178f1f7e558ce41e0
Laxmichaithanya <laxmichaithanya054@gmail.com>
2026-04-02 22:18:48 +0530

    Add files via upload

---
+++ DAY 10.py
@@ -0,0 +1,18 @@
+password = input ("enter the password: ")
+check= set()
+if len(passsword)>=8:
+   for i in  password:
+       if i. islower():
+          check.add('1')
+       elif i.isupper():
+            chaeck.add('u')
+       elif i.supper():
+           chaeck.add('n')
+       else:
+            check.add('s')
+   if len(check)==4:
+        print("weak password")
+ 
+
+    
+
